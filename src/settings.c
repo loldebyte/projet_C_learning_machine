d9fe033d7f332b1ca3d9c8d0aed784bbaf94ffb2
QDIMARELLIS <qdimarellis@laposte.net>
2020-02-09 19:02:46 +0100

    added settings struct & get_current_setttings func

--- src/settings.c
+++ src/settings.c
@@ -9,12 +9,35 @@
 #include <stdbool.h>
 //  RAPPEL : CF format_du_fichier_settings.md POUR LE FORMAT EN LECTURE/ECRITURE
 
+typedef Struct settings {
+    uint8_t taille_police;
+    char carte[10];
+    char fond[10];
+}settings;
+
 void create_default_settings_file(const char * path) {
-    file = fopen(path, "wb");
+    FILE * file = fopen(path, "wb");
     uint8_t taille_police = 20;
     char carte[10] = "white";
     char fond[10]  = "black";
     fwrite(&taille_police, 1, 1, file);
     fwrite(carte, 1, 10, file);
     fwrite(fond, 1, 10, file);
+    fclose(file);
+}
+
+settings get_current_settings(const char * path) {
+    FILE * file = fopen(path, "rb");
+    uint8_t taille_police;
+    char carte[10];
+    char fond[10];
+    fread(taille_police, 1, 1, file);
+    fread(carte, 1, 10, file);
+    fread(fond, 1, 10, file);
+    settings current_settings;
+    current_settings.taille_police = taille_police;
+    current_settings.carte = carte;
+    current_settings.fond = fond;
+    return current_settings;
+    fclose(file);
 }
--- src/settings.h
+++ src/settings.h
@@ -7,5 +7,6 @@ int8_t conf_file_is_valid(const char *);
 void background_color_change(const char *);
 void card_color_change(const char *);
 void font_size_change(const uint8_t);
+settings get_current_settings(const char *);
 
 #endif
